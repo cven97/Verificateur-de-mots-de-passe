e9f56d202911dc5eeabdbe23d06c9b2b69ee6ded
LOMPO <lompolrnt@gmail.com>
2022-03-29 17:54:54 +0000

    first commit

--- PasswordValidator/settings.py
+++ PasswordValidator/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-evate7#5594)&xfe*115_3&%+a)w*9x(0^s($-l!swk*e+m!2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['passeword-verificator.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
